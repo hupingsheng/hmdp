d89adb3ca882a3ad9d0325a9112968b14b7f16d3
hu pingsheng <hu942658636@163.com>
2023-08-12 23:14:37 +0800

    add redis封装工具类

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -2,20 +2,31 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
+import com.hmdp.service.IFollowService;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.CacheClient;
 import com.hmdp.utils.RedisConstants;
+import com.hmdp.utils.RedisData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.ReactiveNumberCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
 
 /**
  * <p>
@@ -31,22 +42,38 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
+    //线程池
+    private static final ExecutorService CACHE_REBUILD_EXECUTOR = Executors.newFixedThreadPool(10);
+
+    @Resource
+    private CacheClient cacheClient;
+
     @Override
     public Result queryById(Long id) {
         //缓存穿透
        // Shop shop = queryWithPassThrough(id);
 
+//        Shop shop = cacheClient
+//                .queryWithPassThrough(RedisConstants.CACHE_SHOP_KEY,id, Shop.class,this::getById,RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
+
        // 互斥锁解决缓存击穿
-        Shop shop = queryWithMutex(id);
+//        Shop shop = queryWithMutex(id);
+
+        //逻辑过期解决缓存击穿
+//        Shop shop = queryWithLogicExpire(id);
+
+        Shop shop = cacheClient
+                .queryWithLogicExpire(CACHE_SHOP_KEY,id, Shop.class,this::getById,20L, TimeUnit.SECONDS);
         if(shop == null){
             return Result.fail("店铺不存在");
         }
         return Result.ok(shop);
     }
 
+
     public Shop queryWithMutex(Long id){
         // 1.从redis查询商铺缓存
-        String shopJson = stringRedisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_KEY + id);
+        String shopJson = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
 
         // 2. 判断是否存在  "abc" isNotBlank是否有实际的字符串
         if(StrUtil.isNotBlank(shopJson)){
@@ -78,54 +105,20 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
             if (shop == null) {
                 //防止缓存穿透，将null值写入redis
-                stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, "", RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
+                stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, "", RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
                 return null;
             }
             //  5. 不存在，返回错误
 
             // 6. 存在，写入redis
-            stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop));
-            stringRedisTemplate.expire(RedisConstants.CACHE_SHOP_KEY + id, RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
+            stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop));
+            stringRedisTemplate.expire(CACHE_SHOP_KEY + id, CACHE_SHOP_TTL, TimeUnit.MINUTES);
         }catch (InterruptedException e){
             throw new RuntimeException(e);
         }finally {
             unlock(lockKey);
         }
 
-        return shop;
-    }
-
-    public Shop queryWithPassThrough(Long id){
-        // 1.从redis查询商铺缓存
-        String shopJson = stringRedisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_KEY + id);
-
-        // 2. 判断是否存在  "abc" isNotBlank是否有实际的字符串
-        if(StrUtil.isNotBlank(shopJson)){
-            Shop shop = JSONUtil.toBean(shopJson,Shop.class);
-            return shop;
-        }
-
-        //命中的是否是空值 ”“
-        if(shopJson != null){
-            return null;
-        }
-        //  3. 存在，直接返回
-
-
-        //  4. 不存在，根据id查询数据库
-        Shop shop = getById(id);
-
-        if(shop == null){
-            //防止缓存穿透，将null值写入redis
-            stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, "", RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
-            return null;
-        }
-        //  5. 不存在，返回错误
-
-        // 6. 存在，写入redis
-        stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop));
-        stringRedisTemplate.expire(RedisConstants.CACHE_SHOP_KEY + id,RedisConstants.CACHE_SHOP_TTL , TimeUnit.MINUTES);
-        // 7.
         return shop;
     }
 
@@ -151,8 +144,21 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 //        1. 先更新数据库，2，删除缓存
         updateById(shop);
 
-        stringRedisTemplate.delete(RedisConstants.CACHE_SHOP_KEY + shop.getId());
+        stringRedisTemplate.delete(CACHE_SHOP_KEY + shop.getId());
 
         return Result.ok();
+    }
+
+
+    public void saveShop2Redis(Long id, Long expireSeconds){
+        //1. 查询店铺数据
+        Shop shop = getById(id);
+
+        // 2. 封装逻辑过期时间
+        RedisData redisData = new RedisData();
+        redisData.setData(shop);
+        redisData.setExpireTime(LocalDateTime.now().plusSeconds(expireSeconds));
+        // 3, 写入redis
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(redisData));
     }
 }
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,34 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
+import com.hmdp.service.impl.ShopServiceImpl;
+
+import com.hmdp.utils.CacheClient;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 
 
+    @Resource
+    private ShopServiceImpl shopService;
+
+    @Autowired
+    private CacheClient cacheClient;
+    @Test
+    public void testSaveShop(){
+        Shop shop = shopService.getById(1L);
+
+        cacheClient.setWithLogicalExpire(CACHE_SHOP_KEY + 1L, shop, 10L, TimeUnit.SECONDS);
+    }
 }
