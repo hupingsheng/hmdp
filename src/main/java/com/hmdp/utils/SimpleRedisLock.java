b276d5126f44ec307993d088ecb63bda3db1d8e1
hu pingsheng <hu942658636@163.com>
2023-08-14 18:10:47 +0800

    add 分布式锁误删问题

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,12 +1,14 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
+import com.hmdp.entity.ShopType;
 import org.omg.CORBA.PRIVATE_MEMBER;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.concurrent.TimeUnit;
 
-public class SimpleRedisLock implements ILock{
+public class SimpleRedisLock implements ILock {
 
     private StringRedisTemplate stringRedisTemplate;
 
@@ -14,26 +16,35 @@ public class SimpleRedisLock implements ILock{
 
     private static final String KEY_PREFIX = "lock:";
 
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.stringRedisTemplate = stringRedisTemplate;
         this.name = name;
     }
 
 
-
     @Override
     public boolean tryLock(long timeoutSec) {
         // 获取线程标识
-        long threadId = Thread.currentThread().getId();
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
         String key = KEY_PREFIX + name;
         //获取锁
-        Boolean success = stringRedisTemplate.opsForValue().setIfAbsent(key, threadId + "", timeoutSec,TimeUnit.SECONDS);
+        Boolean success = stringRedisTemplate.opsForValue()
+                .setIfAbsent(key, threadId, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success);
     }
 
     @Override
     public void unlock() {
-        // 释放锁
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        //获取线程标识
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        // 获取锁的标识
+        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        if (id.equals(threadId)) {
+            // 释放锁
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
+
     }
 }
